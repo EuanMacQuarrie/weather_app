345365eb8f38966a6dc4f24c7d42f5369b1be51d
Euan MacQuarrie <ejmacq@gmail.com>
2020-05-09 23:27:24 -0400

    server and app running and finshed css

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+
 const port = 8000;
+app.listen(port, () => console.log('Listening on port: ${port}'));
+
 /*Body-parser as middle-ware*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -10,6 +13,9 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
 
 app.use(express.static('./website'));
 //get routes for previous entries
@@ -21,9 +27,6 @@ app.get('/', (req, res)=>{
     console.log(`A new entry has been sent: ${projectData}`);
   });
   
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
 app.post('/add', addPost);
 
 function addPost(req, res){
--- website/app.js
+++ website/app.js
@@ -1,15 +1,17 @@
+//api.openweathermap.org/data/2.5/forecast?zip={zip code},{country code}&appid={your api key}
+//api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 //user entry into app
-let zip = document.getElementById('zip').value;
-let place = document.getElementById('province').value;
-let feel = document.getElementById('feelings').value;
+// let zip = document.getElementById('zip').value;
+// let place = document.getElementById('province').value;
+// let feel = document.getElementById('feelings').value;
 
 //parameters of the API
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '&appid=fae203f2da64a84f26e786e6d2e38947';
 
 //new date and time for each instance
-let d = new Date();
-let newDate = d.getMonth() + '.' - d.getDate() + '.' + d.getFullYear();
+const date = new Date();
+const newDate = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
 
 //using API to get weather information
 const getAPI = async (baseURL, zip, apiKey) => {
@@ -30,26 +32,31 @@ document.getElementById('generate').addEventListener('click', getPost);
 
 //Chained promises
 function getPost(e){
-zip = document.getElementById('zip').value;
-place = document.getElementById('province').value;
-feel = document.getElementById('feelings').value;
+    zip = document.getElementById('zip').value;
+    console.log(zip)
+    place = document.getElementById('province').value;
+    console.log(province)
+    feel = document.getElementById('feelings').value;
+    console.log(feelings)
 
-    let url = `${baseURL}zip=${zip},${place}${apiKey}`;
      getAPI(baseURL, zip, apiKey)
-     .then(function(NewData){
-         console.log(NewData)
-         makePost('/add', {temperature: NewData.main.temp, feelings: feel, time: new Date});
+     .then(function(response){
+         console.log(response)
+        //  makePost('/add', {temperature: NewData.main.temp, feelings: feel, time: new Date});
      })
-     .then(function(NewData){
+     .then(function(response){
          updateUI();
      });
     };
 
 //post the data from api
 const makePost = async(url = '/add', data = {})=>{
-    const response = await fetch(url, {
+    const response = await fetch(baseURL + zip + apiKey, {
     method: 'POST',
     credentials: 'same-origin',
+    headers: {
+        'Content-Type': application.json(),
+    },
     body: JSON.stringify(data),
     });
     try 
@@ -62,14 +69,14 @@ const makePost = async(url = '/add', data = {})=>{
         };
 };
 //updating the UI once response is receieved 
-const updateUI = async(url = "/entries")=>{
-    const getEntries = await fetch(url);
+const updateUI = async(url = '/entries')=>{
+    const getEntries = await fetch(baseURL + zip + apiKey);
       try{
         const entries = await getEntries.json()
         console.log(entries);
-        document.getElementById('date').innerHTML = `<span class="far fa-clock"></span> ${entries[0].d}`;
+        document.getElementById('date').innerHTML = `<span class="far fa-clock"></span> ${entries[0].date}`;
         document.getElementById('temp').innerHTML = `<span class="fas fa-sun"></span> ${entries[0].place}°C`;
-        document.getElementById('content').innerHTML = `<span class="fas fa-pencil-alt">Your Previous Entry</span> ${entries[0].feel}`;
+        document.getElementById('content').innerHTML = `<span class="fas fa-pencil-alt"></span> ${entries[0].feel}`;
       }catch(error){
         console.log('An error has occured whilst trying to post: ', error)
       };
