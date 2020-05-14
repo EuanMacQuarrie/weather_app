a208ecbabff5c320229c43dfe5e9dd55df231ac3
Euan MacQuarrie <ejmacq@gmail.com>
2020-05-14 10:27:00 -0400

    further changes made to server but still need to get app.js fully operational

--- server.js
+++ server.js
@@ -17,26 +17,17 @@ app.use(cors());
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-app.use(express.static('./website'));
-//get routes for previous entries
-app.get('/', (req, res)=>{
-    res.sendFile(`./website/index.html`);
-  });
-  app.get('/entries', (req,res)=>{
-    res.send(projectData);
-    console.log(`A new entry has been sent: ${projectData}`);
-  });
-  
-app.post('/add', addPost);
+app.use(express.static('.website'));
 
-function addPost(req, res){
-  let newEntry = {
-    "temperature": req.body.temperature,
-    "feelings": req.body.feelings,
-    "date": req.body.date
-  };
-
-  projectData.unshift(newEntry);
-  console.log(`A new entry added on the server: ${projectData}`);
+// Callback function to complete GET '/all'
+app.get('/all', (req, res) => {
   res.send(projectData);
-};
+});
+
+// Post Route
+app.post('/add', (req, res) => {
+  projectData['date'] = req.body.date;
+  projectData['temp'] = req.body.temp;
+  projectData['content'] = req.body.content;
+  res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,48 +1,41 @@
-//api.openweathermap.org/data/2.5/forecast?zip={zip code},{country code}&appid={your api key}
-//api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
-//user entry into app
-// let zip = document.getElementById('zip').value;
-// let place = document.getElementById('province').value;
-// let feel = document.getElementById('feelings').value;
-
 //parameters of the API
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '&appid=fae203f2da64a84f26e786e6d2e38947';
 
 //new date and time for each instance
-const date = new Date();
-const newDate = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 //using API to get weather information
-const getAPI = async (baseURL, zip, apiKey) => {
-    const response = await fetch (baseURL + zip + apiKey, {
-        method: 'GET',
-});
+function getAPI(e){
+        zipcode = document.getElementById('zip').value;
+        country = document.getElementById('country').value;
+        feel = document.getElementById('feelings').value;
 
-try {
-    const data = await response.json();
-    return data;
-    }
-catch (error){
-    console.log('Whilst making request, there was an error: ',error());
-}};
+    getWeather(baseURL, zipcode, country, apiKey)
+        .then(function(userData){
+            postData('/add', {date: newDate, temp: userData.main.temp, content: content})
+        }).then (function (newData){
+            updateUI
+        })
+};
 
 //event listener
-document.getElementById('generate').addEventListener('click', getPost);
+document.getElementById('generate').addEventListener('click', getAPI);
 
 //Chained promises
 function getPost(e){
     zip = document.getElementById('zip').value;
     console.log(zip)
-    place = document.getElementById('province').value;
-    console.log(province)
+    place = document.getElementById('country').value;
+    console.log(country)
     feel = document.getElementById('feelings').value;
     console.log(feelings)
 
      getAPI(baseURL, zip, apiKey)
      .then(function(response){
          console.log(response)
-        //  makePost('/add', {temperature: NewData.main.temp, feelings: feel, time: new Date});
+         makePost('/add', {temperature: NewData.main.temp, feelings: feel, time: new Date});
      })
      .then(function(response){
          updateUI();
@@ -50,7 +43,7 @@ function getPost(e){
     };
 
 //post the data from api
-const makePost = async(url = '/add', data = {})=>{
+const makePost = async(baseURL, zip, apiKey = '/add', data = {})=>{
     const response = await fetch(baseURL + zip + apiKey, {
     method: 'POST',
     credentials: 'same-origin',
@@ -69,13 +62,13 @@ const makePost = async(url = '/add', data = {})=>{
         };
 };
 //updating the UI once response is receieved 
-const updateUI = async(url = '/entries')=>{
+const updateUI = async(baseURL, zip, apiKey = '/entries')=>{
     const getEntries = await fetch(baseURL + zip + apiKey);
       try{
         const entries = await getEntries.json()
         console.log(entries);
         document.getElementById('date').innerHTML = `<span class="far fa-clock"></span> ${entries[0].date}`;
-        document.getElementById('temp').innerHTML = `<span class="fas fa-sun"></span> ${entries[0].place}°C`;
+        document.getElementById('temp').innerHTML = `<span class="fas fa-sun"></span> ${entries[0].location}°C`;
         document.getElementById('content').innerHTML = `<span class="fas fa-pencil-alt"></span> ${entries[0].feel}`;
       }catch(error){
         console.log('An error has occured whilst trying to post: ', error)
